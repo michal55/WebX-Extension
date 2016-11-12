5214088fb838574d60088108019705af4db1e185
f4rnham <f4rnham@users.noreply.github.com>
2016-11-12 16:12:10 +0100

    Cleanup + add documentation

--- js/popup.js
+++ js/popup.js
@@ -1,78 +1,29 @@
-var HOST = 'http://localhost:3000';
-
-function extractArgFromUrl(url, arg) {
-    var q = url.substr(url.indexOf('#') + 1);
-    var parts = q.split('&');
-    for (var i = 0; i < parts.length; ++i) {
-        var kv = parts[i].split('=');
-        if (kv[0] == arg) {
-            return kv[1];
-        }
-    }
-
-    // Error?
-    return "";
-}
-
-// Launch interactive WebAuthFlow, prompts user for login if necessary
+// Launch interactive dummy API call which will trigger login prompt if necessary
 function loginPrompt() {
-    chrome.identity.launchWebAuthFlow( {
-            'url' : HOST + '/webx/oauth/authorize?client_id=' + chrome.runtime.getManifest().oauth2.client_id +
-                '&redirect_uri=' + encodeURIComponent(chrome.identity.getRedirectURL()) + '&response_type=token&scope=',
-            'interactive' : true
-        },
-        function(redirect_url) {
-            alert(extractArgFromUrl(redirect_url, 'access_token'));
-        });
-}
-
-function xhrWithAuth(method, url, interactive, callback) {
-    var access_token;
-
-    getToken();
-
-    function getToken() {
-        chrome.identity.launchWebAuthFlow( {
-                'url' : HOST + '/webx/oauth/authorize?client_id=' + chrome.runtime.getManifest().oauth2.client_id +
-                '&redirect_uri=' + encodeURIComponent(chrome.identity.getRedirectURL()) + '&response_type=token&scope=',
-                'interactive' : interactive
-            },
-            function(redirect_url) {
-                access_token = extractArgFromUrl(redirect_url, 'access_token');
-                requestStart();
-            });
-    }
-
-    function requestStart() {
-        var xhr = new XMLHttpRequest();
-        xhr.open(method, url);
-        xhr.setRequestHeader('Authorization', 'Bearer ' + access_token);
-        xhr.onload = requestComplete;
-        xhr.send();
-    }
-
-    function requestComplete() {
-        callback(this.status, this.response);
-    }
+    xhrWithAuth('GET', '', true, function(status, response) {
+        if (status != null) {
+            document.getElementById('login').disabled = true;
+        }
+    });
 }
 
 function getUserInfo() {
-    xhrWithAuth('GET',
-        HOST + '/webx/api/data/user',
-        false,
-        onUserInfoFetched);
+    xhrWithAuth('GET', '/data/user', false, onUserInfo);
 }
 
-function onUserInfoFetched(status, response) {
+function onUserInfo(status, response) {
     if (status == 200) {
-        alert(response);
+        alert('OK:\n' + response);
+    } else if (status == null) {
+        document.getElementById('login').disabled = false;
+        alert('Internal error:\n' + response);
     } else {
-        // We failed...
-        alert('RIP ' + status + '\n' + response);
+        alert('HTTP error: ' + status + '\n' + response);
+        document.getElementById('login').disabled = false;
     }
 }
 
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('login').addEventListener('click', loginPrompt);
-    document.getElementById('google').addEventListener('click', getUserInfo);
+    document.getElementById('user_info').addEventListener('click', getUserInfo);
 });
