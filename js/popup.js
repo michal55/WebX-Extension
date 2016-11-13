d34f3f897b838c83a8f141827ed8815a49ff6bf2
michal55 <michal.kren94@gmail.com>
2016-11-14 00:51:32 +0100

    Add button for loading projects and scripts

--- js/popup.js
+++ js/popup.js
@@ -23,7 +23,57 @@ function onUserInfo(status, response) {
     }
 }
 
+function getProjects() {
+    xhrWithAuth('GET', '/data/user/projects', false, onProjects);
+}
+
+function onProjects(status, response) {
+        if (status == 200) {
+            var json = JSON.parse(response);
+            for (var i = 0; i<json.length; i++){
+                var div = document.createElement("DIV");
+                var btn = document.createElement("BUTTON");
+                var text = document.createTextNode(json[i].name);
+
+                div.id = "project" + json[i].id;
+                btn.appendChild(text);
+                btn.addEventListener('click', getScripts(json[i].id));
+                div.appendChild(btn);
+                document.getElementById("projects_div").appendChild(div);          
+
+            }
+        }
+        else
+            alert('Error:\n' + response);
+    }
+
+
+function getScripts(project_id) {
+    xhrWithAuth('GET', '/data/user/project/'+ project_id +'/scripts', false, onScripts);
+    }
+
+function onScripts(status, response) {
+        if (status == 200) {
+            var json = JSON.parse(response);
+            for (var i = 0; i<json.length; i++){
+                var div = document.createElement("DIV");
+                var li = document.createElement("LI");
+                var btn = document.createElement("BUTTON");
+                var text = document.createTextNode(json[i].name);
+
+                div.id = "script" + json[i].id;
+                li.appendChild(btn);
+                btn.appendChild(text);
+                div.appendChild(li);
+
+                document.getElementById("project" + json[i].project_id).appendChild(div);
+            }
+        }    
+}
+
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('login').addEventListener('click', loginPrompt);
     document.getElementById('user_info').addEventListener('click', getUserInfo);
+    document.getElementById('projects').addEventListener('click', getProjects);
+
 });
