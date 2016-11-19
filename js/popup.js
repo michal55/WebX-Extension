bfb5e38e4c76294084da5d2fe0c87026d63fac22
Tomas Juhaniak <juhaniak.tomas@gmail.com>
2016-11-19 21:16:35 +0100

    Upgrade extension logic and design

--- js/popup.js
+++ js/popup.js
@@ -1,11 +1,22 @@
 var app = angular.module('extension', []);
 app.controller('main', function($scope) {
 	$scope.init = function() {
-	    $scope.scripts = false;
-	    $scope.logged_in = false;
-	    $scope.getProjects();
-	    $scope.loginPrompt();
-	    $scope.targeting = false;
+		if(localStorage.logged_in == 'true') {
+			$scope.loginPrompt(); //in case of corrupted cookies
+			$scope.logged_in = true;
+			loadCookies();
+		}
+		else {
+			$scope.logged_in = false;
+		}
+	}
+
+	function loadCookies() {
+		$scope.projects = angular.fromJson(localStorage.projects);
+		$scope.project = angular.fromJson(localStorage.project);
+		$scope.scripts = angular.fromJson(localStorage.scripts);
+		$scope.script = angular.fromJson(localStorage.script);
+		$scope.schema = angular.fromJson(localStorage.schema);
 	}
 
     // Launch interactive dummy API call which will trigger login prompt if necessary
@@ -13,15 +24,29 @@ app.controller('main', function($scope) {
 	    xhrWithAuth('GET', '', true, function(status, response) {
 	        if (status != null) {
 	            $scope.logged_in = true;
+
+	            localStorage.logged_in = true;
+
 	            $scope.$digest();
+
+	            loadCookies();
+	            if ($scope.projects == false) {
+	            	$scope.getProjects();
+	            }
 	        }
 	    });
 	}
 
-	$scope.getUserInfo = function() {
-	    xhrWithAuth('GET', '/data/user', false, function(status, response) {
-		    alert('Hello this is for you');
-		});
+	$scope.logout = function() {
+		$scope.logged_in = false;
+
+		localStorage.logged_in = false;
+		localStorage.projects = false;
+		localStorage.project = false;
+		localStorage.scripts = false;
+		localStorage.script = false;
+		localStorage.schema = false;
+		loadCookies();
 	}
 
 	$scope.getProjects = function() {
@@ -29,9 +54,17 @@ app.controller('main', function($scope) {
 	    	$scope.projects = angular.fromJson(response);
 	    	$scope.project = {name: 'Select project', id: false, disabled: true};
 	    	$scope.projects.unshift($scope.project);
+	    	$scope.scripts = false;
 	    	$scope.script = false;
 	    	$scope.schema = false;
-	    	$scope.$digest();
+
+	    	localStorage.projects = angular.toJson($scope.projects);
+	    	localStorage.project = angular.toJson($scope.project);
+	    	localStorage.scripts = false;
+			localStorage.script = false;
+			localStorage.schema = false;
+
+			$scope.$digest();
 	    });
 	}
 
@@ -40,6 +73,10 @@ app.controller('main', function($scope) {
 	    	$scope.scripts = angular.fromJson(response);
 	    	$scope.script = {name: 'Select script', id: false, disabled: true};
 	    	$scope.scripts.unshift($scope.script);
+
+	    	localStorage.scripts = angular.toJson($scope.scripts);
+			localStorage.script = angular.toJson($scope.script);
+
 	    	$scope.$digest();
 	    });
 	}
@@ -47,8 +84,19 @@ app.controller('main', function($scope) {
 	$scope.getSchema = function() {
 		xhrWithAuth('GET', '/data/user/project/'+ $scope.project.id.toString() +'/data_schemas', false, function(status, response) {
 	    	$scope.schema = angular.fromJson(response);
+
+			localStorage.schema = angular.toJson($scope.schema);
+
 	    	$scope.$digest();
 	    });
+	}
+
+	$scope.changeScript = function() {
+		localStorage.schema = angular.toJson($scope.schema);
+	}
+
+	$scope.saveScript = function() {
+		console.log(angular.toJson($scope.schema));
 	}
 
 	$scope.toggleTargeting = function() {
@@ -65,5 +113,15 @@ app.controller('main', function($scope) {
 			$('body').css('height', '300px');
 		}
 
+	}
+
+	$scope.selectProject = function() {
+		localStorage.project = angular.toJson($scope.project);
+		$scope.getScripts();
+		$scope.getSchema();
+	}
+
+	$scope.selectScript = function() {
+		localStorage.script = angular.toJson($scope.script);
 	}
 });
