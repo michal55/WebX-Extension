50e383f781a6522436620fc60134f320ddca9f0a
f4rnham <f4rnham@users.noreply.github.com>
2017-02-19 15:36:52 +0100

    Add options page with server selection

---
+++ js/options.js
@@ -0,0 +1,32 @@
+var app = angular.module('extension', []);
+app.controller('main', function($scope) {
+    $scope.init = function() {
+        // Default options
+        $scope.DEFAULT_SERVER_URL = 'http://localhost:3000/webx';
+
+        $scope.loading = true;
+
+        chrome.storage.sync.get({
+            server_url: $scope.DEFAULT_SERVER_URL
+        }, function(items) {
+            $scope.server_url = items.server_url;
+            $scope.loading = false;
+            $scope.$digest();
+        });
+    };
+
+    $scope.optionsChanged = function() {
+        chrome.storage.sync.set({
+            server_url: $scope.server_url
+        }, function() {
+            // Update status to let user know options were saved
+            $scope.status = 'Options saved';
+            $scope.$digest();
+
+            setTimeout(function() {
+                $scope.status = '';
+                $scope.$digest();
+            }, 750);
+        });
+    };
+});
